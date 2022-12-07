7663a38f664c765f2acc62329d98ff5efede18af
gitmsx <gitmsx@mail.ru>
2022-12-07 23:33:51 +0300

    Saved on 07.12.2022-23:33:50,91

--- Assets/Scripts/nis.cs
+++ Assets/Scripts/nis.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
@@ -14,7 +15,16 @@ public class nis : MonoBehaviour
     [SerializeField] private float _jumpForce;
 
 
+
+    [HideInInspector] private Text Text__info003;
+    [HideInInspector] private Text Text__info001;
+    [HideInInspector] private Text Text__info002;
+
     
+
+
+
+
     private VadIS _VadIS;
 
 
@@ -42,12 +52,27 @@ public class nis : MonoBehaviour
     {
         _rigidbody=GetComponent<Rigidbody>();
         _VadIS.Player.Shoot.performed += context => Short2();
+
+        Text__info001 = GameObject.Find("Text__info001").GetComponent<Text>();
+        Text__info002 = GameObject.Find("Text__info002").GetComponent<Text>();
+        Text__info003 = GameObject.Find("Text__info003").GetComponent<Text>();
+        Text__info003.text = "Text__info003";
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        Vector2 vec2 = _VadIS.Player.Move.ReadValue<Vector2>();
+        Vector3 force2 = new Vector3(vec2.x, 0f, vec2.y) * _force*100;
+        _rigidbody.AddRelativeForce(force2);
+
+        Text__info003.text = force2.ToString();
+        Text__info001.text = vec2.x.ToString();
+        Text__info002.text = vec2.y.ToString();
+
     }
 
 
@@ -59,7 +84,7 @@ public class nis : MonoBehaviour
     }
 
 
-    void FixedUpdate()
+    void FixedUpdate2()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
