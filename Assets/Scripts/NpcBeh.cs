bbda61c4902561838c2bf5683227c3b288bee974
gitmsx <gitmsx@mail.ru>
2022-12-08 09:13:43 +0300

    Saved on 08.12.2022- 9:13:42,92

---
+++ Assets/Scripts/NpcBeh.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class NpcBeh : MonoBehaviour
+{
+
+    Rigidbody rb;
+    [SerializeField]    GameObject Signal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Signal.SetActive(false);
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Signal.SetActive(true);
+        print(other.gameObject.tag);
+        print("********************************* OnTriggerEnter ************************");
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Signal.SetActive(true);
+        print(collision.gameObject.tag);
+        print("=============================== OnCollisionEnter");
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        print(gameObject);
+        Signal.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    
+        {
+
+    }
+}
